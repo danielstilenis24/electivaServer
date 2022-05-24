e33b6e3fbba9e84098dedaccf189737203c3948e
Daniel Lenis <daniel.lenis@isoptimo.com>
2022-05-23 19:08:10 -0500

    Commit inicial para subir proyecto server

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors")
+const app = express();
+const { API_VERSION } = require("./config");
+
+const userRoutes = require("./src/routes/user");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors())
+/* Creación de los endpoint del proyecto */
+app.use(`/api/${API_VERSION}`, userRoutes);
+
+/* Condiguración de los header HTTP */
+module.exports = app;
